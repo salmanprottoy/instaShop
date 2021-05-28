bfe1d034f58b6cbc7ed9d1e5ea4f1ba296018a9e
TanvirAhmed177 <ahmed.tanvir.siddik@gmail.com>
2021-05-28 23:57:28 +0600

    Second

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">git merge [source branch] [target branch]</div>;
+  return <div className="App">Hello</div>;
 }
 
 export default App;
