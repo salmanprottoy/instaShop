067eaedc618f027cde1797971e4c89a6baeedec3
Sarman7105 <sarman7105@gmail.com>
2021-05-29 21:18:38 +0600

    dev tool package added

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,9 @@
 import { createStore, combineReducers } from "redux";
 import ProductsReducer from "./reducers/ProductsReducer";
+import { devToolsEnhancer } from 'redux-devtools-extension';
 
 const root = combineReducers({
   ProductsReducer,
 });
-const store = createStore(root);
+const store = createStore(root,devToolsEnhancer);
 export default store;
