7cd26113b0f96be1622c2f9e04acf09a6cc51031
Sarman7105 <sarman7105@gmail.com>
2021-05-29 21:36:36 +0600

    CartReducer added

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers } from "redux";
 import ProductsReducer from "./reducers/ProductsReducer";
+import CartReducer from "./reducers/CartReducer";
 import { devToolsEnhancer } from 'redux-devtools-extension';
 
 const root = combineReducers({
   ProductsReducer,
+  CartReducer,
 });
-const store = createStore(root,devToolsEnhancer);
+const store = createStore(root,devToolsEnhancer());
 export default store;
---
+++ src/store/reducers/CartReducer.js
@@ -0,0 +1,12 @@
+const initState = {
+    products: [],
+    totalPrice: 0,
+    totalQuantities: 0
+}
+const CartReducer = (state = initState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }    
+}
+export default CartReducer;
