38cb46619c3a27fcf0d33ed202c5f7bf59dc6340
Sarman7105 <sarman7105@gmail.com>
2021-05-30 00:30:05 +0600

    cart component modified

--- src/store/reducers/CartReducer.js
+++ src/store/reducers/CartReducer.js
@@ -4,6 +4,8 @@ const initState = {
     totalQuantities: 0
 }
 const CartReducer = (state = initState, action) => {
+    let findPro;
+    let index;
     switch (action.type) {
         case 'ADD_TO_CART':
             const { product, quantity } = action.payload;
@@ -20,6 +22,43 @@ const CartReducer = (state = initState, action) => {
                     ...state,products:[...state.products,product],totalPrice:Tprice,totalQuantities:Tquantities
                 }
             }
+        case 'INC':
+            findPro = state.products.find(product => product.id === action.payload);
+            index = state.products.findIndex(product => product.id === action.payload)
+            findPro.quantity += 1;
+            state.products[index] = findPro;
+            return {
+                ...state,
+                totalPrice: state.totalPrice + findPro.discountPrice,
+                totalQuantities:state.totalQuantities+1
+            }
+        case 'DEC':
+            findPro = state.products.find(product => product.id === action.payload);
+            index = state.products.findIndex(product => product.id === action.payload)
+            if (findPro.quantity>1) {
+                findPro.quantity -= 1;
+                state.products[index] = findPro;
+                return {
+                ...state,
+                totalPrice: state.totalPrice - findPro.discountPrice,
+                totalQuantities:state.totalQuantities-1
+            }
+            }
+            else {
+                return state;
+            }
+        
+        case 'REMOVE':
+            findPro = state.products.find(product => product.id === action.payload);
+            const filtered= state.products.filter(product=>product.id!==action.payload)
+            
+            return {
+                ...state,
+                products: filtered,
+                totalPrice: state.totalPrice - findPro.discountPrice * findPro.quantity,
+                totalQuantities:state.totalQuantities-findPro.quantity
+            }
+            
         default:
             return state;
     }    
