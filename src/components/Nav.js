0eb7c88524d275d10bc3ee23ab9ff9ade9b3818d
Md. Salman Hossan Prottoy <salman.prottoy@gmail.com>
2021-05-29 16:22:25 +0600

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BsFillBagFill } from "react-icons/bs";
-import { BrowserRouter as Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
